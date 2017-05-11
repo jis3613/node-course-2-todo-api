36361bfd0899babed7cc22ea26cadf7a1a734adc
jis3613 <jis3613@gmail.com>
2017-05-11 13:06:29 +0200

    Typo fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(proces.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(procces.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
