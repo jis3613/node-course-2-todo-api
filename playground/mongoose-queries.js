24d8aa0fa652f71b4c0cf024b6ede0a9bee3e24f
jis3613 <jis3613@gmail.com>
2017-05-11 12:15:10 +0200

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -4,31 +4,31 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} =require('./../server/models/user');
 
-// var id = '59142519ec63110f0664ec78';
+var id = '59142519ec63110f0664ec78';
 
-// if(!ObjectID.isValid(id)) {
-//   console.log('ID not valid');
-// }
+if(!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
 
-//mongoosejs.com 항상 참조할것
-// Todo.find({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos', todos);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('Todo',todo);
-// });
+// mongoosejs.com 항상 참조할것
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
 
-// Todo.findById(id).then((todo) => {
-//   if(!todo){
-//     return console.log('Id not found');
-//   }
-//   console.log('Todo by ID',todo);
-// }).catch((e) => console.log(e))
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo',todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if(!todo){
+    return console.log('Id not found');
+  }
+  console.log('Todo by ID',todo);
+}).catch((e) => console.log(e))
 
 User.findById('5913345ea0705f08630cc151').then((user) => {
   if (!user){
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,6 +34,25 @@ app.get('/todos',(req,res) => {
     res.send({todos});
   },(e)=>{
     res.status(400).send(e);
+  });
+});
+
+//GET /todos/1234324
+app.get('/todos/:id',(req,res) =>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    //{todo}=={todo: todo}
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
